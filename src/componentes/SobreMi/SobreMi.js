57f1ff710a6ab7805258d940be8bcad207eeb586
LucasGlave <lucasglave7@gmail.com>
2024-02-29 15:06:34 -0300

    add button cliente and sobre mi

--- src/componentes/SobreMi/SobreMi.js
+++ src/componentes/SobreMi/SobreMi.js
@@ -1,31 +1,24 @@
 import './SobreMi.scss'
+import React from 'react'
 import Reveal from '../Reveal';
-import Typewriter from '../Typewriter';
 
-const SobreMi = () =>{
-    return(
-        <section id='SOBREMI'>
-            <div className='div-img'>
-                <img src='./IMG_3330.webp' alt='imgS'className='imgS'/>
-                <img src='./IMG_3331.webp' alt='imgS'className='imgResponsive'/> 
-            </div>
-            <Reveal>
-                <div className='sm-content'>
-                    <div className='content-left'>
-                        <div className='typewriter'>
-                            <h1>DON BASILIO</h1>
-                            <Typewriter />
-                        </div>
-                    </div>
-                    <div className='content-right'>
-                        <button className='button1'>SACAR TURNO</button>
-                        <button className='button2'>CLIENTES</button>
-                    </div>
-                </div>
+const SobreMi = () => {
+  return (
+    <section id='SOBREMI'>
+        <div className='fix'>
+        <h2>- SOBRE MI -</h2>
+            <Reveal className='cardSM'>
+                <h4>Mi nombre es <span>Alan Muñoz</span> y tengo 28 años. Soy un peluquero con experiencia y pasión por mi trabajo. Desde hace 9 años, he estado cortando el pelo en Darregueira, un pequeño pueblo de la provincia de Buenos Aires en Argentina.
+                    Trabajar como peluquero en Darregueira me ha permitido conocer a muchas personas interesantes y variadas. Algunos son clientes habituales que vienen a verme cada mes para mantener su cabello en buena forma, mientras que otros son nuevos en la ciudad y buscan un lugar confiable para recibir un corte de pelo fresco y moderno.
+                    La peluquería donde trabajo es más que un simple negocio para mí. Es un espacio donde puedo ejercer mi creatividad y habilidades para crear peinados y cortes personalizados para cada cliente. Me encanta hablar con mis clientes mientras les corto el pelo y conocer sus historias y experiencias de vida. Muchas veces, estas conversaciones se convierten en amistades duraderas.
+                    Además de ser un peluquero en Darregueira, también he participado en eventos de moda y peluquería en toda la provincia, lo que me ha permitido expandir mis habilidades y mantenerme actualizado con las últimas tendencias y estilos.
+                    Cortar el pelo es mi pasión y me enorgullece poder hacer mi trabajo con excelencia en Darregueira. No solo estoy agradecido por tener una clientela fiel, sino que también disfruto de la sensación de ser parte de una comunidad unida y amistosa. Espero seguir creciendo como peluquero y seguir cortando el pelo en Darregueira por muchos años más.
+                </h4>
             </Reveal>
-            
-        </section>
-    )
+        </div>
+        
+    </section>
+  )
 }
 
 export default SobreMi
